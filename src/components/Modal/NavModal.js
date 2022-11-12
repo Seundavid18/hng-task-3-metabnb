b5a2e545cc7872f712b074a714cf375f694376fb
unknown <oyewumidavid84@gmail.com>
2022-11-12 19:38:14 +0100

    style: fixed navbar modal

---
+++ src/components/Modal/NavModal.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import fox from '../../images/fox.png'
+import wallet from '../../images/wallet.png'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+function NavModal() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <button className="btn nav-btn" type="submit" variant="primary" onClick={handleShow}>Connect Wallet</button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Connect Wallet</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <h4 className='walletTxt'>Choose your preferred wallet:</h4>
+            <ul className='wallet'>
+                <li className='walletItem mb-3'>
+                    <img src={fox} alt="" className='walletImg'/>
+                    <span className='walletMetaText'>Metamask</span>
+                    <div className='ms-auto'>
+                        <ArrowForwardIosIcon style={{color: "#959DA6"}}/>
+                    </div>
+                </li>
+                <li className='walletItem mb-3'>
+                    <img src={wallet} alt="" className='walletImg'/>
+                    <span className='walletMetaText'>WalletConnect</span>
+                    <div className='ms-auto'>
+                        <ArrowForwardIosIcon style={{color: "#959DA6"}}/>
+                    </div>
+                </li>
+            </ul>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
+
+export default NavModal;
+
--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -4,6 +4,7 @@ import logo from '../../images/Logo.png'
 import fox from '../../images/fox.png'
 import wallet from '../../images/wallet.png'
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import NavModal from '../Modal/NavModal'
 
 const Topbar = () => {
   return (
@@ -32,36 +33,7 @@ const Topbar = () => {
                     <a className="nav-link navbarItem me-3 text-dark">Community</a>
                     </li>
                 </ul>
-                <button className="btn nav-btn" type="submit" data-bs-toggle="modal" data-bs-target="#walletModal">Connect Wallet</button>
-                <div className="modal fade" id="walletModal" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
-                    <div className="modal-dialog modal-dialog-centered">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h1 className="modal-title fs-5" id="exampleModalToggleLabel">Connect wallet</h1>
-                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div className="modal-body">
-                                <h4 className='walletTxt'>Choose your preferred wallet:</h4>
-                                <ul className='wallet'>
-                                    <li className='walletItem mb-3'>
-                                        <img src={fox} alt="" className='walletImg'/>
-                                        <span className='walletMetaText'>Metamask</span>
-                                        <div className='ms-auto'>
-                                            <ArrowForwardIosIcon style={{color: "#959DA6"}}/>
-                                        </div>
-                                    </li>
-                                    <li className='walletItem mb-3'>
-                                        <img src={wallet} alt="" className='walletImg'/>
-                                        <span className='walletMetaText'>WalletConnect</span>
-                                        <div className='ms-auto'>
-                                            <ArrowForwardIosIcon style={{color: "#959DA6"}}/>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <NavModal />
             </div>
         </div>
     </nav>
