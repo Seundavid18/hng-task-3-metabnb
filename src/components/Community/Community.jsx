b5b2abbdd7dd30f7ec87ec22291a836f8043c911
unknown <oyewumidavid84@gmail.com>
2022-11-13 13:33:05 +0100

    style: fixed buttons

--- src/components/Community/Community.jsx
+++ src/components/Community/Community.jsx
@@ -6,13 +6,13 @@ const Community = () => {
   return (
     <div className='community'>
       <div className='container mt-5 mb-5'>
-            <div className='row row-cols-1 row-cols-lg-2 align-items-center g-5'>
-                <div className='col col-lg-5'>
+            <div className='d-lg-flex flex-nowrap gap-5'>
+                <div className='flex-meta'>
                     <h1 className='comTitle'>Metabnb NFTs</h1>
                     <p className='comDesc'>Discover our NFT gift cards collection. Loyal customers gets amazing gift cards which are traded as NFTs. These NFTs gives our cutomer access to loads of our exclusive services.</p>
                     <button className='comBtn'>Learn more</button>
                 </div>
-                <div className='col col-lg-7'>
+                <div className='flex-box'>
                     <img src={comImg} alt="" className='comImg'/>
                 </div>
             </div>
